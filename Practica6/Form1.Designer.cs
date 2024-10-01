bd7a0235227ae5759b139f3e85d0489d861bf00d
Jose Clemente <jose_clemente_corzo@hotmail.com>
2024-10-01 01:46:37 -0600

    Agregar archivos de proyecto.

---
+++ Practica6/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Practica6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bnEncender = new System.Windows.Forms.Button();
+            this.bnApagar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bnEncender
+            // 
+            this.bnEncender.Location = new System.Drawing.Point(302, 102);
+            this.bnEncender.Name = "bnEncender";
+            this.bnEncender.Size = new System.Drawing.Size(179, 108);
+            this.bnEncender.TabIndex = 0;
+            this.bnEncender.Text = "Encender";
+            this.bnEncender.UseVisualStyleBackColor = true;
+            this.bnEncender.Click += new System.EventHandler(this.bnEncender_Click);
+            // 
+            // bnApagar
+            // 
+            this.bnApagar.Location = new System.Drawing.Point(302, 263);
+            this.bnApagar.Name = "bnApagar";
+            this.bnApagar.Size = new System.Drawing.Size(179, 64);
+            this.bnApagar.TabIndex = 1;
+            this.bnApagar.Text = "Apagar";
+            this.bnApagar.UseVisualStyleBackColor = true;
+            this.bnApagar.Click += new System.EventHandler(this.bnApagar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bnApagar);
+            this.Controls.Add(this.bnEncender);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.CerrandoForm1_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bnEncender;
+        private System.Windows.Forms.Button bnApagar;
+    }
+}
+
---
+++ Practica6/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+
+namespace Practica6
+{
+    public partial class Form1 : Form
+    {
+        public SerialPort ArduinoPort { get; } //ArduinoPort es personalizable
+        public Form1()
+        {
+            InitializeComponent();
+            ArduinoPort = new System.IO.Ports.SerialPort();
+            ArduinoPort.PortName = "COM5";
+            ArduinoPort.BaudRate = 9600;
+            ArduinoPort.Open();
+
+            this.FormClosing += CerrandoForm1_Click;
+            this.bnApagar.Click += bnApagar_Click;
+            this.bnEncender.Click += bnEncender_Click;
+
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bnEncender_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("b");
+        }
+
+        private void bnApagar_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("a");
+
+        }
+
+        private void CerrandoForm1_Click(object sender, EventArgs e)
+        {
+            if (ArduinoPort.IsOpen) ArduinoPort.Close();
+        }
+    }
+}
